81d548dcad322ec9c048af8633144549e6b20fb9
gjsrn5698 <gjsrn5698@naver.com>
2024-05-07 18:27:14 +0900

    2024.5.7. 마지막 코딩

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,33 @@
-import Image from 'next/image'
+import Image from "next/image";
+import getAuth from "@/auth";
+import LoginButton from "@/app/components/LoginButton";
+import LogoutButton from "@/app/components/LogoutButton";
 
-export default function Home() {
-  return <p>Hello World!</p>;
+export default async function Home() {
+  const auth = getAuth();
+  const user = await auth.user();
+
+
+  return (
+      <div className="flex w-full max-w-xl flex-col items-center border bg-white p-10 text-left">
+        {user ? (
+            <>
+                <h2 className="mb-8 font-bold">Hello, {user.name || user.email}!</h2>
+                <Image
+                    src={user.photo}
+                    alt="User Profile"
+                    width={96}
+                    height={96}
+                    className="mb-10 h-24 w-24 rounded-full object-cover"
+                />
+                <LogoutButton />
+            </>
+        ) : (
+            <>
+              <p className="mb-4">Sign in to get started</p>
+              <LoginButton />
+            </>
+        )}
+      </div>
+  );
 }
